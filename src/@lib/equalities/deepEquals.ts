20abb076facdf9a7fdfbea574e888441daf1df5f
Jin Kwan Kim <kkole3897@gmail.com>
2025-04-06 17:02:53 +0900

    feat: deepEquals 구현

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,28 @@
+import { isObject } from "../../utils/types";
+
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+
+    return objA.every((value, index) => deepEquals(value, objB[index]));
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+      return false;
+    }
+
+    return keysA.every((key) => deepEquals(objA[key], objB[key]));
+  }
+
+  return false;
 }
