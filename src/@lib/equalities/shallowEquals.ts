83335461a16e505f9f78aa7bd54497ae33120eb3
Jin Kwan Kim <kkole3897@gmail.com>
2025-04-06 17:00:56 +0900

    feat: shallowEquals 구현

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,28 @@
+import { isObject } from "../../utils/types";
+
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (isObject(objA) && isObject(objB)) {
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+      return false;
+    }
+
+    return keysA.every((key) => objA[key] === objB[key]);
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+
+    return objA.every((value, index) => value === objB[index]);
+  }
+
+  return false;
 }
